67a3fb5323f1fffd572846bfe336a01cf6b0df81
Piyush Padia <piyush.padia2021@vitstudent.ac.in>
2025-01-30 09:35:40 +0530

    changes

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -109,7 +109,7 @@ const Chat = () => {
 
       {/* Right Side */}
       <div className="Right-side-chat" >
-        <div  className="nav-icons-container">
+        <div  className="nav-icons-container" >
           <NavIcons />
         </div>
         <ChatBox
